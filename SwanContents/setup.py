1902f7eacfd873311fea22ec954a22402ec5567a
Omar Zapata <Omar.Zapata@cern.ch>
2020-07-29 16:00:50 +0200

    SwanContents v0.0.8

--- SwanContents/setup.py
+++ SwanContents/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='swancontents',
-      version='0.0.7',
+      version='0.0.8',
       description='Jupyter Notebook filesystem with Projects',
       include_package_data=True,
       packages=find_packages(),
